8169cb30e28b07fe14f5485fb202ee9e7d1d4042
98kimjh <98kimjh@gmail.com>
2021-12-06 16:40:41 +0900

    Copy oop.point.v3 package to oop.point.v4

---
+++ javaex/src/com/javaex/oop/point/v4/Point.java
@@ -0,0 +1,42 @@
+package com.javaex.oop.point.v4;
+
+// v1. 필드, getter/setter
+public class Point {
+	private int x;
+	private int y;
+	
+	public Point() {}
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void draw()	{
+		System.out.printf("점[x=%d, y=%d]을 그렸습니다.%n", x, y);
+	}
+	
+	public void draw(boolean bDraw) {
+		String message = String.format("점[x=%d, y=%d]을 ", x, y);
+		if (bDraw) {
+			message += "그렸습니다.";
+		} else {
+			message += "지웠습니다.";
+		}
+		System.out.println(message);
+	}
+	
+}
---
+++ javaex/src/com/javaex/oop/point/v4/PointApp.java
@@ -0,0 +1,19 @@
+package com.javaex.oop.point.v4;
+
+public class PointApp {
+
+	public static void main(String[] args) {
+		Point p1 = new Point();
+		p1.setX(10);
+		p1.setY(20);
+		p1.draw();
+		p1.draw(true);
+		p1.draw(false);
+		
+		Point p2 = new Point(30, 40);
+		p2.draw();
+		p2.draw(true);
+		p2.draw(false);
+	}
+
+}
