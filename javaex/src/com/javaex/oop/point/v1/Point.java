ecc4093f69dad9019287f4637cba795e3edc1469
98kimjh <98kimjh@gmail.com>
2021-12-06 11:49:17 +0900

    Point getter/setter 세팅

--- javaex/src/com/javaex/oop/point/v1/Point.java
+++ javaex/src/com/javaex/oop/point/v1/Point.java
@@ -1,5 +1,25 @@
 package com.javaex.oop.point.v1;
 
+// v1. 필드, getter/setter
 public class Point {
-
+	private int x;
+	private int y;
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void draw()	{
+		System.out.printf("점[x=%d, y=%d]을 그렸습니다.%n", x, y);
+	}
+	
 }
--- javaex/src/com/javaex/oop/point/v1/PointApp.java
+++ javaex/src/com/javaex/oop/point/v1/PointApp.java
@@ -3,8 +3,15 @@ package com.javaex.oop.point.v1;
 public class PointApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Point p1 = new Point();
+		p1.setX(10);
+		p1.setY(20);
+		p1.draw();
+		
+		Point p2 = new Point();
+		p2.setX(30);
+		p2.setY(40);
+		p2.draw();
 	}
 
 }
