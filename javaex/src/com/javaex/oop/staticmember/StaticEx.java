b2aa51083da9781ebc049928b606ff222aefc9e9
98kimjh <98kimjh@gmail.com>
2021-12-06 14:41:14 +0900

    Init StaticEx

---
+++ javaex/src/com/javaex/oop/staticmember/StaticEx.java
@@ -0,0 +1,5 @@
+package com.javaex.oop.staticmember;
+
+public class StaticEx {
+
+}
---
+++ javaex/src/com/javaex/oop/staticmember/StaticExApp.java
@@ -0,0 +1,10 @@
+package com.javaex.oop.staticmember;
+
+public class StaticExApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
