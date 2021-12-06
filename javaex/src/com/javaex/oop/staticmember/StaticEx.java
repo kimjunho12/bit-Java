960afdff6accf8461c4550d531091cdeed313c28
98kimjh <98kimjh@gmail.com>
2021-12-06 15:14:15 +0900

    Static & Instance variable Exercise

--- javaex/src/com/javaex/oop/staticmember/StaticEx.java
+++ javaex/src/com/javaex/oop/staticmember/StaticEx.java
@@ -1,5 +1,32 @@
 package com.javaex.oop.staticmember;
 
 public class StaticEx {
-
+	public static int refCount;		// 클래스(static) 멤버
+	public static String classVar;	// 클래스 멤버
+	public String instanceVar;		// 인스턴스 멤버
+	
+	// static 블록으로 static 영역 초기화
+	static {
+		refCount = 0;
+		classVar = "Static Member";
+//		instanceVar = "Instance Member";	// 접근 불가
+		System.out.println("Static Block");
+	}
+	
+	// 생성자
+	StaticEx() {
+		// 참조 카운트 증가
+		// 인스턴스 맴버 -> static 멤버 접근은 가능
+		refCount++;
+		System.out.println("인스턴스 개수:" + refCount);
+		System.out.println("생성자 호출: 인스턴스 생성");
+	}
+	
+	// 소멸자
+	@Override
+	protected void finalize() throws Throwable {
+		refCount--;
+		System.out.println("소멸자 호출");
+		super.finalize();
+	}
 }
--- javaex/src/com/javaex/oop/staticmember/StaticExApp.java
+++ javaex/src/com/javaex/oop/staticmember/StaticExApp.java
@@ -3,8 +3,27 @@ package com.javaex.oop.staticmember;
 public class StaticExApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("RefCount:" + StaticEx.refCount);
+		StaticEx s1 = new StaticEx();
+		// static 멤버는 new없이 접근 가능
+		System.out.println("RefCount:" + StaticEx.refCount);
+		
 
+		StaticEx s2 = new StaticEx();
+		System.out.println("RefCount:" + StaticEx.refCount);
+		
+		s2 = null;		// s2 해제
+		System.out.println("s2 해제");
+		System.out.println("RefCount: "+StaticEx.refCount);
+		
+		// ! 주의 : 가비지 컬렉터를 직접 호출하지 않는다
+		System.gc();
+		try {
+			Thread.sleep(3000);
+			System.out.println("RefCount: "+StaticEx.refCount);
+		} catch (Exception e) {
+			
+		}
 	}
 
 }
