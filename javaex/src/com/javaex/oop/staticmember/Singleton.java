20b66a0ab694fd84b7ba7bbb88e189644cf7fb1d
98kimjh <98kimjh@gmail.com>
2021-12-06 16:39:58 +0900

    Complete Singleton Exercise

--- javaex/src/com/javaex/oop/staticmember/Singleton.java
+++ javaex/src/com/javaex/oop/staticmember/Singleton.java
@@ -1,5 +1,18 @@
 package com.javaex.oop.staticmember;
 
+// 목표 : 어떤 상황에서든 단 한개의 인스턴스만 유지
 public class Singleton {
-
+	
+	// static 블록에서 생성자 호출
+	private static Singleton instance = new Singleton();
+	// 생성자
+	private Singleton() {
+		
+	}
+	
+	// getter
+	public static Singleton getInstance() {
+		return instance;
+	}
+	
 }
--- javaex/src/com/javaex/oop/staticmember/SingletonApp.java
+++ javaex/src/com/javaex/oop/staticmember/SingletonApp.java
@@ -4,7 +4,11 @@ public class SingletonApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Singleton s1 = Singleton.getInstance();
+		Singleton s2 = Singleton.getInstance();
+				
+		// 객체 주소가 같으면 같은 객체
+		System.out.println("s1, s2가 같은 객체? " + (s1 == s2));
 	}
 
 }
