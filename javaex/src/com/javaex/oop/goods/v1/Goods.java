c920baf133475b1ccd9939a3867ed90cfd9e6899
98kimjh <98kimjh@gmail.com>
2021-12-06 11:33:42 +0900

    default type class field 접근

--- javaex/src/com/javaex/oop/goods/v1/Goods.java
+++ javaex/src/com/javaex/oop/goods/v1/Goods.java
@@ -1,10 +1,7 @@
 package com.javaex.oop.goods.v1;
 
 public class Goods {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	// 필드 작성
+	String name;
+	int price;
 }
--- javaex/src/com/javaex/oop/goods/v1/GoodsAPP.java
+++ javaex/src/com/javaex/oop/goods/v1/GoodsAPP.java
@@ -4,7 +4,16 @@ public class GoodsAPP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Goods notebook = new Goods();
+		notebook.name = "Macbook Air";
+		notebook.price = 1600000;
+		
+		Goods smartphone = new Goods();
+		smartphone.name = "iPhone 13";
+		smartphone.price = 1000000;
 
+		System.out.printf("%s, %d원%n", notebook.name, notebook.price);
+		System.out.printf("%s, %d원%n", smartphone.name, smartphone.price);
 	}
 
 }
