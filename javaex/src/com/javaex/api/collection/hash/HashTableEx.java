4e1543bec0daee8332d40fad7421a8014d76bf17
98kimjh <98kimjh@gmail.com>
2021-12-08 17:38:41 +0900

    Enumeration Test

--- javaex/src/com/javaex/api/collection/hash/HashTableEx.java
+++ javaex/src/com/javaex/api/collection/hash/HashTableEx.java
@@ -1,5 +1,6 @@
 package com.javaex.api.collection.hash;
 
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Iterator;
@@ -11,6 +12,7 @@ public class HashTableEx {
 		Map<String, Integer> map = new Hashtable<String, Integer>();
 //		Hashtable<String, Integer> map = new Hashtable<String, Integer>();
 //		Map<String, Integer> map = new HashMap<String, Integer>();
+//		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		
 		// 키-값 쌍으로 저장
 		// 맵에 데이터 넣기
@@ -26,6 +28,7 @@ public class HashTableEx {
 		System.out.println("SOCCER 키가 있는가? " + map.containsKey("SOCCER"));
 		// 값 포함 여부
 		System.out.println("값에 9가 있는가? " + map.containsValue(9));
+		
 		
 		// Iterator
 		Iterator<String> it = map.keySet().iterator();
