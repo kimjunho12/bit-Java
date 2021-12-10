10be8ed608f2f158389d741a29dcaa2ec8d2f27b
98kimjh <98kimjh@gmail.com>
2021-12-10 10:53:26 +0900

    Outer / Inner Class

---
+++ First/src/com/ds/practice/test/MemberInnerTest.java
@@ -0,0 +1,34 @@
+package com.ds.practice.test;
+
+import com.ds.practice.test.Outter.Inner;
+
+class Outter {
+	
+	private int num = 10;
+	private static int num2 = 3;
+	
+	class Inner {
+		public void disp() {
+			System.out.println(num);
+			System.out.println(num2);
+		}
+	}
+	
+}
+
+
+public class MemberInnerTest {
+
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Outter o = new Outter();
+//		
+//		Inner i = o.new Inner();
+		
+
+		Inner i = new Outter().new Inner();
+		i.disp();
+
+	}
+
+}
