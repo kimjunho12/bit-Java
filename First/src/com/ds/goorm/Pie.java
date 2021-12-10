c2fbbeb99fcafc4c24a01bb38ddc964493771c2f
98kimjh <98kimjh@gmail.com>
2021-12-10 10:53:07 +0900

    원주율 소숫점 n자리까지의 출력

---
+++ First/src/com/ds/goorm/Pie.java
@@ -0,0 +1,16 @@
+package com.ds.goorm;
+
+import java.io.*;
+
+public class Pie {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		
+		final String PIE = "3.14159265358979323846";			// π 값 저장
+		int pieLen = Integer.valueOf(input);
+		
+		
+		System.out.print(String.valueOf(PIE.toCharArray(), 0, 2+pieLen));	// 소숫점 자리수와 3.을 위한 2자리 추가 후 출력
+	}
+}
