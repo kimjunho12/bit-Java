05a9013a711e4ea0caca85540fd2f801d8181518
98kimjh <98kimjh@gmail.com>
2021-12-21 10:07:36 +0900

    tryresource

--- jdbc_proj/src/com/mysql/level1/SelectData1.java
+++ jdbc_proj/src/com/mysql/level1/SelectData1.java
@@ -13,15 +13,16 @@ public class SelectData1 {
 		} catch (ClassNotFoundException cnfe) {
 			System.out.println("해당 클래스를 찾을 수 없습니다." + cnfe.getMessage());
 			return;
-		} 
+		}
 		String url = "jdbc:mysql://localhost:3306/jdbcdb?characterEncoding=UTF-8&serverTimezone=UTC";
 		String user = "root";
 		String passwd = "1234";
-		try (Connection conn = DriverManager.getConnection(url, user, passwd);Statement stmt = conn.createStatement()){
-			ResultSet rs = stmt.executeQuery("select * from student");
-			while(rs.next()) {
-				System.out.println(rs.getString("name")+"학생은 "+rs.getInt("score")+"점 입니다.");
-			}				
+		try (Connection conn = DriverManager.getConnection(url, user, passwd);		// try/resource 구문
+				Statement stmt = conn.createStatement()) {							// try에서 생성한 변수 resource는 블럭이 끝나면 자동 close	
+			ResultSet rs = stmt.executeQuery("select * from student");				// but closable interface를 구현한 class만 생성가능
+			while (rs.next()) {
+				System.out.println(rs.getString("name") + "학생은 " + rs.getInt("score") + "점 입니다.");
+			}
 		} catch (SQLException se) {
 			System.out.println(se.getMessage());
 		}
