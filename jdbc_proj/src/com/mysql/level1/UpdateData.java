4af2fa1b89d44ac1523ce313f3aa017cbe45b08f
98kimjh <98kimjh@gmail.com>
2021-12-21 14:00:56 +0900

    update DB

--- jdbc_proj/src/com/mysql/level1/UpdateData.java
+++ jdbc_proj/src/com/mysql/level1/UpdateData.java
@@ -13,23 +13,23 @@ public class UpdateData {
 		} catch (ClassNotFoundException cnfe) {
 			System.out.println("해당 클래스를 찾을 수 없습니다." + cnfe.getMessage());
 			return;
-		} 
+		}
 		String url = "jdbc:mysql://localhost:3306/jdbcdb?characterEncoding=UTF-8&serverTimezone=UTC";
 		String user = "root";
 		String passwd = "1234";
 		try (Connection conn = DriverManager.getConnection(url, user, passwd);
 				PreparedStatement pstmt = conn.prepareStatement("update student set score = ? where name = ?");
-				Scanner scan = new Scanner(System.in);){
+				Scanner scan = new Scanner(System.in);) {
 			System.out.print("학생 이름을 입력하세요 : ");
-		    String name = scan.nextLine();
+			String name = scan.nextLine();
 			System.out.print("학생 점수를 입력하세요 : ");
 			int score = Integer.parseInt(scan.nextLine());
-			pstmt.setInt(1,  score);
-		    pstmt.setString(2, name);
+			pstmt.setInt(1, score);
+			pstmt.setString(2, name);
 			int updateNum = pstmt.executeUpdate();
-			System.out.println("student 테이블에서 " +updateNum + "행 변경 완료");						
+			System.out.println("student 테이블에서 " + updateNum + "행 변경 완료");
 		} catch (SQLException se) {
 			System.out.println(se.getMessage());
-		} 
+		}
 	}
 }
