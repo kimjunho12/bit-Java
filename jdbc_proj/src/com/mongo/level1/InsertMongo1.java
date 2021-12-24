b2517f00f6bcdadbec3bb2fee0595fe82dd9f5c2
Juno <98kimjh@gmail.com>
2021-12-24 14:58:18 +0900

    insert Document only one

--- jdbc_proj/src/com/mongo/level1/InsertMongo1.java
+++ jdbc_proj/src/com/mongo/level1/InsertMongo1.java
@@ -1,33 +1,39 @@
 package com.mongo.level1;
 
-import java.util.function.Consumer;
-
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.bson.Document;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
 public class InsertMongo1 {
-    public static void main(String[] args) {
-        try {
-            MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+	public static void main(String[] args) {
+		Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+		mongoLogger.setLevel(Level.SEVERE);
+		try {
+            MongoClient mongoClient = new MongoClient();
             MongoDatabase db = mongoClient.getDatabase("edudb");
             MongoCollection<Document> collection = db.getCollection("book");
-
-            Document doc = new Document("name", "spring").append("price", "30000");
+            
+           Document doc = new Document("name", "spring")
+                    .append("price", "30000");
             collection.insertOne(doc);
-
-            Consumer<Document> printConsumer = new Consumer<Document>() {
-                @Override
-                public void accept(final Document document) {
-                    System.out.println(document.toJson());
-                }
-            };
-            collection.find().forEach(printConsumer);
+            
+            MongoCursor<Document>  cursor  = collection.find().iterator();
+            while(cursor.hasNext()) {
+            	Document doc1 = cursor.next();
+            	System.out.println(doc1.get("name") + " : " + doc1.get("price"));           
+            }      
             mongoClient.close();
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
+        } catch (Exception exception) {
+            System.err.println(exception.getClass().getName() + ": " + exception.getMessage());
         }
-    }
+	}
 }
+
+
