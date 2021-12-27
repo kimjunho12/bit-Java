1985ca57ac848e1e69fce890f07a4f59dab678ce
98kimjh <98kimjh@gmail.com>
2021-12-27 16:21:38 +0900

    servlet 예제

---
+++ edu/src/main/java/FirstServlet.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>FirstServlet 수행완료</h1>");
+		out.close();
+		System.out.println("서블릿에서의 표준 출력은 어디로 나갈까요?");
+		
+	}
+
+}
