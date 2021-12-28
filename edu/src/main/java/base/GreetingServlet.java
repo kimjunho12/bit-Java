20640535c8f4c9db901340584e52809df1fec8ae
98kimjh <98kimjh@gmail.com>
2021-12-28 12:04:15 +0900

    Greeting 종료

--- edu/src/main/java/base/GreetingServlet.java
+++ edu/src/main/java/base/GreetingServlet.java
@@ -1,25 +1,28 @@
 package base;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class GreetingServlet
- */
+
 @WebServlet("/greeting")
 public class GreetingServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print(String.format("<h2> %s 님 반가워요.. 오늘은 %s요일입니다!!</h2>",
+				request.getParameter("guestname") == null ? "손님" : request.getParameter("guestname"),
+				LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.NARROW, Locale.KOREAN)));
 	}
 
 }
