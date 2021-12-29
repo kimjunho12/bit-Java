904d8a7108a602989bbc29b15475a74520ad8d22
98kimjh <98kimjh@gmail.com>
2021-12-29 10:26:15 +0900

    image with a tag

---
+++ edu/src/main/java/base/baseBasketServlet.java
@@ -0,0 +1,26 @@
+package base;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/basket")
+public class baseBasketServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		String pid = request.getParameter("pid");
+		out.printf("<h1>선택한 상품 : %s</h1>", pid);
+		out.printf("<img src='/edu/images/%d.jpg'><hr>", Integer.valueOf(pid.substring(pid.length()-3, pid.length())));
+		out.print("<a href='/edu/htmlexam/productlog.html'>상품 선택 화면</a>");
+	}
+}
