5a2d38c08b3b47c2b414a653be455cbb96998a23
98kimjh <98kimjh@gmail.com>
2021-12-29 15:39:03 +0900

    rename BasketServlet.java

--- edu/src/main/java/base/BasketServlet.java
+++ edu/src/main/java/base/BasketServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 
 @WebServlet("/basket")
-public class baseBasketServlet extends HttpServlet {
+public class BasketServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
