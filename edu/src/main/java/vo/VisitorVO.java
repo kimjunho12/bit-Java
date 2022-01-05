d530688a138e7338fb06d41edca796dc1d6cba99
98kimjh <98kimjh@gmail.com>
2022-01-05 15:21:00 +0900

    게시판 데이터 모델 구현 (VO/DAO/Connecter)

---
+++ edu/src/main/java/vo/VisitorVO.java
@@ -0,0 +1,36 @@
+package vo;
+
+public class VisitorVO {			// VO (Value Object) ⇋ DTO (Data Transfer Object) ⇋ Entity
+	private int id;	
+	private String name;
+	private String writeDate;
+	private String memo;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(String writeDate) {
+		this.writeDate = writeDate;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	@Override
+	public String toString() {
+		return "VisitorVO [id=" + id + ", memo=" + memo + ", name=" + name + ", writeDate=" + writeDate + "]";
+	}
+}
