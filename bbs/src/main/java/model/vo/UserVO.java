6ad4e93062156f2a1b61951897840f702fbcfb6e
98kimjh <98kimjh@gmail.com>
2022-01-07 00:56:24 +0900

    rs 생성자 추가

--- bbs/src/main/java/model/vo/UserVO.java
+++ bbs/src/main/java/model/vo/UserVO.java
@@ -1,5 +1,8 @@
 package model.vo;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserVO {
 	private int uid;
 	private String name;
@@ -8,52 +11,79 @@ public class UserVO {
 	private String phone;
 	private String register_date;
 	private String last_date;
-	
+
+	public UserVO() {
+	}
+
+	public UserVO(ResultSet rs) throws SQLException {
+		this.uid = rs.getInt("uid");
+		this.name = rs.getString("name");
+		this.email = rs.getString("email");
+		this.pw = rs.getString("pw");
+		this.phone = rs.getString("phone");
+		this.register_date = rs.getString("register_date");
+		this.last_date = rs.getString("last_date");
+	}
+
 	public int getUid() {
 		return uid;
 	}
+
 	public void setUid(int id) {
 		this.uid = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPw() {
 		return pw;
 	}
+
 	public void setPw(String pw) {
 		this.pw = pw;
 	}
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public String getRegister_date() {
 		return register_date;
 	}
+
 	public void setRegister_date(String register_date) {
 		this.register_date = register_date;
 	}
+
 	public String getLast_date() {
 		return last_date;
 	}
+
 	public void setLast_date(String last_date) {
 		this.last_date = last_date;
 	}
+
 	@Override
 	public String toString() {
 		return "UserVO [id=" + uid + ", name=" + name + ", email=" + email + ", pw=" + pw + ", phone=" + phone
 				+ ", register_date=" + register_date + ", last_date=" + last_date + "]";
-	}	
+	}
 }
