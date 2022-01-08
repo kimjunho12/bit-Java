ae114fea023f0b6d06c37bd14eed136cb6204f80
98kimjh <98kimjh@gmail.com>
2022-01-08 18:45:01 +0900

    request 인코딩 전처리

---
+++ bbs/src/main/java/filter/CharEncoding.java
@@ -0,0 +1,55 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class CharEncoding
+ */
+@WebFilter(dispatcherTypes = {DispatcherType.REQUEST }
+					, description = "request.setCharacterEncoding('UTF-8');", urlPatterns = { "/*" })
+public class CharEncoding implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public CharEncoding() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		System.out.println("한글필터 실행");
+		if (((HttpServletRequest) request).getMethod().equals("POST"))
+			request.setCharacterEncoding("UTF-8");
+		
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
