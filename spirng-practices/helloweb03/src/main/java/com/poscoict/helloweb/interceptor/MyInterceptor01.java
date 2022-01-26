24ba14fb91f8af75021dc23d94ffeb293b182b33
98kimjh <98kimjh@gmail.com>
2022-01-26 14:59:52 +0900

    [helloweb03] Interceptor Exercise

---
+++ spirng-practices/helloweb03/src/main/java/com/poscoict/helloweb/interceptor/MyInterceptor01.java
@@ -0,0 +1,30 @@
+package com.poscoict.helloweb.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class MyInterceptor01 implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("MyInterceptor01.preHandler(...) called");
+		return false;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("MyInterceptor01.postHandler(...) called");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		System.out.println("MyInterceptor01.afterCompletion(...) called");
+	}
+
+}
