ebabf654cc55351e4104bebdcc0890c55d9cbe90
98kimjh <98kimjh@gmail.com>
2022-02-10 14:35:50 +0900

    [myapplication] springboot-configuration & test #01

--- springboot-practices/myapplication/src/main/java/ex01/MyApplication.java
+++ springboot-practices/myapplication/src/main/java/ex01/MyApplication.java
@@ -1,11 +1,11 @@
 package ex01;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
 public class MyApplication {
 
 	@Bean
--- springboot-practices/myapplication/src/test/java/ex01/MyApplicationTest02.java
+++ springboot-practices/myapplication/src/test/java/ex01/MyApplicationTest02.java
@@ -3,12 +3,10 @@ package ex01;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+/* @Configuration 설정 클래스는 자동으로 찾지 못함 */
 //@ExtendWith(SpringExtension.class)
 //@ContextConfiguration(classes = {MyApplication.class})
 @SpringBootTest
@@ -16,7 +14,7 @@ public class MyApplicationTest02 {
 
 	@Autowired
 	private MyComponent myComponent;
-	
+
 	@Test
 	public void myComponentNotNull() {
 		assertNotNull(myComponent);
