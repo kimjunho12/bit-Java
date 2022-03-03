08149ede1b362043898c0ad42ea114a6d7757b22
98kimjh <98kimjh@gmail.com>
2022-03-03 16:59:39 +0900

    [node-practices/project-ex02] require /w localpath

---
+++ node-practices/project-ex02/app-ex01.js
@@ -0,0 +1,9 @@
+/**
+ * posco-ict-math 파일 모듈 테스트
+ */
+
+var poscoMath = require("../posco-ict-math/posco-ict-math");
+
+console.log(poscoMath.sum(10, 20, 30, 40, 50));
+console.log(poscoMath.max(10, 20, 30, 40, 50));
+console.log(poscoMath.min(10, 20, 30, 40, 50));
