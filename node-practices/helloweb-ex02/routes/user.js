71875eaa8eb2a4a0785566e031f1206a5f869e8d
98kimjh <98kimjh@gmail.com>
2022-03-04 10:33:52 +0900

    [node/helloweb-ex02] express & ejs #02 send api

--- node-practices/helloweb-ex02/routes/user.js
+++ node-practices/helloweb-ex02/routes/user.js
@@ -1,3 +1,4 @@
+const { json } = require('express');
 const express = require('express');
 
 const router = express.Router();
@@ -5,6 +6,38 @@ const router = express.Router();
 
 module.exports = router;
 
-// router.route('').get(function(req, res) {
-//     res.render("main/index");
-// });
+router.route('').get(function(req, res) {
+    res.render("user/info", {
+        no: req.query.no || 0
+    });
+});
+
+router.route('/join').get(function(req, res) {
+    res.render("user/join");
+});
+
+router.route('/join').post(function(req, res) {
+    console.log(req.body);
+    res.redirect("/");
+});
+
+router.route('/api').get(function(req, res) {
+    vo = {
+        no: 10,
+        name: '둘리',
+        email: 'dooly@gmail.com',
+        gender: 'male'
+    };
+
+    // res.writeHead(200, {
+    //     'Content-Type': 'application/json'
+    // })
+    // res.end(JSON.stringify(vo));
+    res.send(vo);
+});
+
+router.route('/:no').get(function(req, res) {
+    res.render("user/info", {
+        no: req.params.no || 0
+    });
+});
