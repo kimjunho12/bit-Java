b1d0f36f3c99d7b8ab400f10b30967be5cab0871
98kimjh <98kimjh@gmail.com>
2022-03-14 12:44:21 +0900

    [react/component] Styling /w Normal CSS {module:true}

---
+++ react-practices/2.Component/ex02/src/03/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./assets/css/App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1
+        className={"Header"}
+      >{`Normal CSS (css loader options : {module: true})`}</h1>
+    </div>
+  );
+};
+
+export default App;
