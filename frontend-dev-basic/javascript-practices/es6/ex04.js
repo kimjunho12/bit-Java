12a8defbda5be0e85c4b8ac75d7e97b493b48d4b
98kimjh <98kimjh@gmail.com>
2022-03-14 13:25:31 +0900

    [javascript/es6] function default parameter

---
+++ frontend-dev-basic/javascript-practices/es6/ex04.js
@@ -0,0 +1,10 @@
+/**
+ * Default Parameter
+ */
+
+const print = function (strs, end = "\n") {
+  console.log(strs.join(end));
+};
+
+print(["Always", "with", "me"]);
+print(["Always", "with", "me"], " ");
