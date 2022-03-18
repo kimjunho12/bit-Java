9f7541db11cfd348b5e45d0bba40ab215f45bcd0
98kimjh <98kimjh@gmail.com>
2022-03-18 17:25:07 +0900

    [react/component] kanbanboard #07 : checkbox

--- react-practices/kanbanboard/src/TaskList.js
+++ react-practices/kanbanboard/src/TaskList.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./assets/css/TaskList.css";
 
 const TaskList = ({ tasks }) => {
+  const [checked, setChecked] = useState(tasks.done);
+  const onChangeCheckBox = (e) => {
+    setChecked(e.target.checked);
+  };
+
   return (
     <li className={styles.TaskList__Task}>
-      <input type="checkbox" defaultChecked={tasks.done} />
+      <input type="checkbox" checked={checked} onChange={onChangeCheckBox} />
       {" " + tasks.name + " "}
       {/* <a href="#" className={styles["TaskList__Task--remove"]}></a> */}
       <a href="#" className={styles["TaskList__Task__remove"]}></a>
