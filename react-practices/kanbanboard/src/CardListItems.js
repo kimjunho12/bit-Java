e66252e8d2b9fb9f52e5619c29d87cbceef9d0be
98kimjh <98kimjh@gmail.com>
2022-03-18 17:24:53 +0900

    [react/component] kanbanboard #06 : openclose

--- react-practices/kanbanboard/src/CardListItems.js
+++ react-practices/kanbanboard/src/CardListItems.js
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import TaskList from "./TaskList";
 
 import styles from "./assets/css/Card.css";
 
 const CardListItems = ({ card }) => {
+  const [isOpend, setIsOpened] = useState(true);
+
+  const openClose = (e) => {
+    setIsOpened(!isOpend);
+  };
+
+  const openDiv = (
+    <div className={styles.Card__Details}>
+      {card.description}
+
+      <div className="TaskList">
+        <ul>
+          {card.tasks.map((task) => (
+            <TaskList key={task.no} tasks={task} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+
   return (
     <div className={styles.Card}>
-      <div className={styles.Card__Title}>{card.title}</div>
-      <div className={styles.Card__Details}>
-        {card.description}
-
-        <div className="TaskList">
-          <ul>
-            {card.tasks.map((task) => (
-              <TaskList key={task.no} tasks={task} />
-            ))}
-          </ul>
-        </div>
+      <div
+        className={isOpend ? styles.Card__Title : styles.Card__Title__open}
+        onClick={openClose}
+      >
+        {card.title}
       </div>
+      {isOpend ? openDiv : null}
     </div>
   );
 };
