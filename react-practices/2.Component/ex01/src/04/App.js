8c00e9f4e3eb0d77bc35292b39d3df6cec4913ad
98kimjh <98kimjh@gmail.com>
2022-03-21 11:54:00 +0900

    [react/component] validation #02

--- react-practices/2.Component/ex01/src/04/App.js
+++ react-practices/2.Component/ex01/src/04/App.js
@@ -4,7 +4,17 @@ import MyComponent from "./MyComponent";
 const App = () => {
   return (
     <div id="App">
-      <MyComponent props01={"Hello World!"} props02={2} props03={true} />
+      <MyComponent
+        props01={"Hello World!"}
+        props02={2}
+        props03={true}
+        props04={{ no: 1 }}
+        props05={[1, 2, 4, 5]}
+        props06={() => "함수"}
+        props07={1234}
+        props08={[false, true, false]}
+        props09={{ no: "tt", name: "test", email: "test@email.com" }}
+      />
     </div>
   );
 };
--- react-practices/2.Component/ex01/src/04/MyComponent.js
+++ react-practices/2.Component/ex01/src/04/MyComponent.js
@@ -1,7 +1,17 @@
 import React, { Fragment } from "react";
 import { PropTypes } from "prop-types";
 
-export default function MyComponent({ props01, props02, props03 }) {
+export default function MyComponent({
+  props01,
+  props02,
+  props03,
+  props04,
+  props05,
+  props06,
+  props07,
+  props08,
+  props09,
+}) {
   return (
     <Fragment>
       <h2>Property Validation</h2>
@@ -10,6 +20,32 @@ export default function MyComponent({ props01, props02, props03 }) {
       <span>props02 : {props02 ? props02 : "--- Not Set ---"}</span>
       <br />
       <span>props03 : {props03 ? `${props03}` : "--- Not Set ---"}</span>
+      <br />
+      <span>
+        props04 : {props04 ? JSON.stringify(props04) : "--- Not Set ---"}
+      </span>
+      <br />
+      <span>
+        props05 :{" "}
+        {props05
+          ? props05.map((e, i) => <b key={i}>{e} </b>)
+          : "--- Not Set ---"}
+      </span>
+      <br />
+      <span>props06 : {props06 ? props06() : "--- Not Set ---"}</span>
+      <br />
+      <span>props07 : {props07 ? props07 : "--- Not Set ---"}</span>
+      <br />
+      <span>
+        props08 :{" "}
+        {props08
+          ? props08.map((e, i) => <b key={i}>{`${e}`} </b>)
+          : "--- Not Set ---"}
+      </span>
+      <br />
+      <span>
+        props09 : {props09 ? JSON.stringify(props09) : "--- Not Set ---"}
+      </span>
       <br />
     </Fragment>
   );
@@ -20,6 +56,18 @@ MyComponent.propTypes = {
   props01: PropTypes.string,
   props02: PropTypes.number.isRequired,
   props03: PropTypes.bool.isRequired,
+  props04: PropTypes.object.isRequired,
+  props05: PropTypes.array.isRequired,
+  props06: PropTypes.func.isRequired,
+
+  // PropTypes Validator (combined primitive)
+  props07: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  props08: PropTypes.arrayOf(PropTypes.bool).isRequired,
+  props09: PropTypes.shape({
+    no: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+  }),
 };
 
 // default value
@@ -27,4 +75,10 @@ MyComponent.defaultProps = {
   props01: "기본 값",
   props02: 222,
   props03: false,
+  props04: {},
+  props05: [],
+  props06: () => {},
+  props07: "문자 및 숫자 가능",
+  props08: [1, 2, 3, 4],
+  props09: { no: 333, name: "ss", email: "ss@email.com" },
 };
