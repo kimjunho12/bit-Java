c998f1adca40a0bf174395ffb7a8bff8dd03d8c7
98kimjh <98kimjh@gmail.com>
2022-03-31 09:54:31 +0900

    [react/routing] #01 : manual hash router

--- react-practices/03.routing/src/01/App.js
+++ react-practices/03.routing/src/01/App.js
@@ -1,8 +1,40 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import Main from "./component/Main";
 import Gallery from "./component/Gallery";
 import Guestbook from "./component/Guestbook";
 
 export default function App() {
-    return (<div>Hash Route 직접 만들어 보기</div>);
+  const [route, setRoute] = useState("");
+  const handleHashChange = () => {
+    const route = window.location.hash.substring(1);
+    console.log(route);
+    setRoute(route);
+  };
+  useEffect(() => {
+    window.addEventListener("hashchange", handleHashChange);
+
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+
+  return (() => {
+    // return에 if문을 쓸 수 없어서 함수를 만들어서 그안에서 분기 후 리턴
+    switch (route) {
+      case "/":
+        return <Main />;
+        break;
+      case "/main":
+        return <Main />;
+        break;
+      case "/guestbook":
+        return <Guestbook />;
+        break;
+      case "/gallery":
+        return <Gallery />;
+      default:
+        return null;
+        break;
+    }
+  })();
 }
