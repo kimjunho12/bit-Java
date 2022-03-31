f6e43065ba245bfd9a4cb4a4f59126f2d6322ece
98kimjh <98kimjh@gmail.com>
2022-03-31 11:29:14 +0900

    [react/routing] #04 : Link vs. NavLink

--- react-practices/03.routing/README.md
+++ react-practices/03.routing/README.md
@@ -3,7 +3,7 @@
 ### [ex01](./src/01/App.js): React Router Basic I: Implemented in a Simple Way(hashchange event driven)
 ### [ex02](./src/02/App.js): React Router Basic II: HTML5 history API
 ### [ex03](./src/03/App.js): React Router Basic III: HashRouter &amp; BrowserRouter
-### ex04: React Router Basic V: Link VS NavLink
+### [ex04](./src/04/App.js): React Router Basic V: Link VS NavLink
 ### ex05: Styling &amp; Semantic Markup
 ### ex06: Composition
 ### ex07: Nesting Routes
--- react-practices/03.routing/src/04/component/Main.js
+++ react-practices/03.routing/src/04/component/Main.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import {Link, NavLink} from 'react-router-dom';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Main() {
-    return (
-        <div>
-            <h1>Main</h1>
-
-        </div>
-    );
+  return (
+    <div>
+      <h1>Main</h1>
+      {/* <ul>
+        <li>
+          <Link to={"/"}>[Main]</Link>
+        </li>
+        <li>
+          <Link to={"/gallery"}>[Gallery]</Link>
+        </li>
+        <li>
+          <Link to={"/guestbook"}>[Guestbook]</Link>
+        </li>
+      </ul> */}
+      <ul>
+        <li>
+          <NavLink to={"/"}>[Main]</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/gallery"}>[Gallery]</NavLink>
+        </li>
+        <li>
+          <NavLink to={"/guestbook"}>[Guestbook]</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
