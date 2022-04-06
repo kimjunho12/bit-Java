d9906a1d2e6ec085ce592ad34297018bca49aea5
98kimjh <98kimjh@gmail.com>
2022-04-06 10:59:01 +0900

    [mysite07] CI/CD settings

---
+++ react-practices/mysite07/backend/src/main/java/me/kickscar/mysite/controller/LandingController.java
@@ -0,0 +1,20 @@
+package me.kickscar.mysite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LandingController {
+	
+	@RequestMapping("")
+	public String index() {
+		return "index";
+	}
+	
+	@RequestMapping("favicon.ico")
+	@ResponseBody
+	public void returnNoFavicon() {
+		
+	}
+}
